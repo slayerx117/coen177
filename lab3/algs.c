8b2c717267e4978cd0abefb09acc4d614ade653b
slayerx117 <benjaminaaron117@att.net>
2019-10-30 20:50:53 -0700

    huh

--- lab3/algs.c
+++ lab3/algs.c
@@ -1,15 +1,1 @@
-voidd sc(Node* pageTable, int page){
-	//find first node without lifeline and replace
-	Node* temp = pageTable->head;
-	while(temp->next != NULL){
-		if(temp->sc == 0){
-			updatePage(temp, page);
-			free(temp);
-			return;
-		}
-		updateSC(temp, 0);
-	}
-	free(temp);
-	fprintf(stderr, "something went wrong in sc");
-	exit(-1);
-	}
+fifo()fifo
